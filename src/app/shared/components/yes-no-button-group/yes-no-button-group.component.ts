1d2df7637e63442b2a078b34b9862a2d8aa567e9
Diego Minelli <diego.minelli@hotmail.com>
2022-05-02 19:56:51 -0300

    garantindo id unicos

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -7,6 +7,7 @@ import {
   Output,
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -24,11 +25,14 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   @Input() public value: string = null;
   @Input() public label = '';
   @Output() public valueChange = new EventEmitter<string>();
+  public id: string = null;
   public options = YesNoButtonGroupOptions;
   public onChange = (value: string) => {};
   public onTouched = () => {};
 
-  constructor() {}
+  constructor() {
+    this.id = `yes-no-button-group-${uuid.v1()}`;
+  }
 
   ngOnInit(): void {}
 
