24edacbbacf7acf2e85d8d76ce4e72c2d2d38586
Diego Minelli <diego.minelli@hotmail.com>
2022-05-02 21:31:10 -0300

    usando o Keyboard ManagerItem Directive

--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -1,13 +1,24 @@
-import { Directive, HostListener } from '@angular/core';
+import { KeyboardManagerItemDirective } from './keyboard-manager-item.directive';
+import {
+  ContentChildren,
+  Directive,
+  HostListener,
+  QueryList,
+} from '@angular/core';
 
 @Directive({
   selector: '[appKm]',
 })
 export class KeyboardManagerDirective {
+  @ContentChildren(KeyboardManagerItemDirective)
+  public items: QueryList<KeyboardManagerItemDirective> = null;
+
   @HostListener('keyup', ['$event'])
   public managerKeys(event: KeyboardEvent): void {
     switch (event.key) {
       case 'ArrowUp':
+        console.log(this.items);
+
         console.log('up');
         break;
       case 'ArrowDown':
