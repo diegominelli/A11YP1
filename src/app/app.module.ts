fb52a6145b3fe84069f5714fd19ae9260f26925c
Diego Minelli <diego.minelli@hotmail.com>
2022-05-05 18:51:37 -0300

    usando o disable control directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DisableControlModule } from './shared/directives/keyboard-manager/disabled-control/disable-control.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     YesNoButtonGroupModule,
     ReactiveFormsModule,
     FormsModule,
+    DisableControlModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
